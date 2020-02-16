70dd023e2f2ef7e165eb0b992cecaa5955316215
ImranSasudin <imransasudin@gmail.com>
2020-02-16 22:56:22 +0800

    Teacher view questions

--- app/src/main/java/com/example/molekular/Adapter/QuestionAdapter.java
+++ app/src/main/java/com/example/molekular/Adapter/QuestionAdapter.java
@@ -1,5 +1,6 @@
 package com.example.molekular.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,6 +8,7 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,40 +16,41 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.molekular.EditNotesActivity;
 import com.example.molekular.Model.Notes;
+import com.example.molekular.Model.Question;
 import com.example.molekular.R;
 import com.example.molekular.ViewNotesActivity;
 
 import java.util.List;
 
-public class QuestionAdapter {
+public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.MyHolder> {
 
-    /*List<Notes> noteslist;
+    List<Question> questionList;
     private Context context;
-    public NotesAdapter(List<Notes> noteslist, Context context){
+    public QuestionAdapter(List<Question> questionList, Context context){
         this.context = context;
-        this.noteslist = noteslist;
+        this.questionList = questionList;
     }
 
     @NonNull
     @Override
-    public NotesAdapter.MyHolder onCreateViewHolder (@NonNull ViewGroup viewGroup, int i){
+    public QuestionAdapter.MyHolder onCreateViewHolder (@NonNull ViewGroup viewGroup, int i){
         View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.notes_row,viewGroup,false);
+                .inflate(R.layout.question_row,viewGroup,false);
 
-        NotesAdapter.MyHolder myHolder = new NotesAdapter.MyHolder(view);
+        QuestionAdapter.MyHolder myHolder = new QuestionAdapter.MyHolder(view);
         return myHolder;
     }
 
     @Override
-    public void onBindViewHolder (@NonNull NotesAdapter.MyHolder myHolder, int position) {
-        Notes notes = noteslist.get(position);
+    public void onBindViewHolder (@NonNull QuestionAdapter.MyHolder myHolder, int position) {
+        Question question = questionList.get(position);
 
-        myHolder.title.setText(notes.getTitle());
+        myHolder.title.setText(question.getQuestion());
         //myHolder.description.setText(notes.getDescription());
     }
 
     @Override
-    public int getItemCount() { return noteslist.size();}
+    public int getItemCount() { return questionList.size();}
 
     class MyHolder extends RecyclerView.ViewHolder {
         TextView title;
@@ -59,26 +62,34 @@ public class QuestionAdapter {
                 @Override
                 public void onClick(View view) {
 
-                    Notes notes = noteslist.get(getAdapterPosition());
+                    Question question = questionList.get(getAdapterPosition());
 
-                    final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
-                    String role = sharedPref.getString("role", "");
+                    String choice1 = question.choice1;
+                    String choice2 = question.choice2;
+                    String choice3 = question.choice3;
+                    String choice4 = question.choice4;
+                    String correctAnswer = question.correctAnswer;
 
-                    Intent i = null;
-                    if (role.equalsIgnoreCase("student")) {
-                        i = new Intent(context, ViewNotesActivity.class);
-                    } else if (role.equalsIgnoreCase("teacher")) {
-                        i = new Intent(context, EditNotesActivity.class);
-                    }
+                    String str = "<html><body><ul><li>" + choice1 + "</li><li>" + choice2 + "</li><li>" + choice3 + "</li><li>" + choice4 + "</li></ul><br>Correct Answer: " + correctAnswer;
+                    // create a WebView with the current stats
+                    WebView webView = new WebView(context);
+                    webView.loadDataWithBaseURL(null, str, "text/html", "utf-8", null);
 
+                    // display the WebView in an AlertDialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Answers")
+                            .setView(webView)
+                            .setNeutralButton("OK", null)
+                            .show();
+                    /*Intent i = new Intent(context, EditNotesActivity.class);
                     i.putExtra("id", notes.id);
                     i.putExtra("title", notes.title);
                     i.putExtra("description", notes.description);
 
-                    context.startActivity(i);
+                    context.startActivity(i);*/
 
                 }
             });
         }
-    }*/
+    }
 }
--- app/src/main/java/com/example/molekular/QuizActivity.java
+++ app/src/main/java/com/example/molekular/QuizActivity.java
@@ -1,20 +1,82 @@
 package com.example.molekular;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.molekular.Adapter.NotesAdapter;
+import com.example.molekular.Adapter.QuestionAdapter;
+import com.example.molekular.Model.Notes;
+import com.example.molekular.Model.Question;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
-
+    ProgressDialog progressDialog;
+    RecyclerView recyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    List<Question> question =new ArrayList<>();
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_questions);
+
+        progressDialog = new ProgressDialog(this);
+        // Loading bar
+        progressDialog.setMessage("Questions Loading, Please Wait...");
+        progressDialog.show();
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(QuizActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                ((LinearLayoutManager) layoutManager).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        final QuestionAdapter questionAdapter = new QuestionAdapter(question, this);
+        recyclerView.setAdapter(questionAdapter);
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference("Questions");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Question listQuestion = dataSnapshot1.getValue(Question.class);
+                    question.add(listQuestion);
+                }
+                progressDialog.dismiss();
+                questionAdapter.notifyDataSetChanged();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getBaseContext(), "Database ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
 
